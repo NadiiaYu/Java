1beff9938bb61c9bf8c682cd0bff38005c8ba363
NadiiaYu <nadiia.yurchenko.81@gmail.com>
2022-10-19 23:01:46 -0400

    Added FizzBuzz class 2

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class FizzBuzzTest {
+
+    //   1. Positive testing Happy path
+    //   if (start <= end) {
+    //    return array;
+
+    //start < end
+    @Test
+    public void testStartLessThanEnd_HappyPath() {
+        FizzBuzz fb = new FizzBuzz();
+    }
+
+    //   2. Nagative testing
+    //   if (start <= end) {
+    //return new String[0];
+
+}
